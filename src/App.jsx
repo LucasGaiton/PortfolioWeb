4f2e7992a3cae47aac977fc14b1d12dc55035e73
lucasemanuel <gaitonlucas7@gmail.com>
2023-10-09 05:39:01 -0300

    Agregamos foto

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,29 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
 import ParticlesBackground from './components/ParticlesBackground'
+//importamos el estilo 
+import S from "../src/App.module.css"
+import image from "../Imagenes/WhatsApp Image 2023-09-20 at 12.33.20 PM.jpeg"
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div>
-      <ParticlesBackground />
-      <button onClick={()=> setCount(count + 1)}>Hola</button>
-      <p>{count}</p>
-      
+    <div className={S.app}>
+      <div className='Conteiner1'>
+        <div className='foto'>
+          <img src={image} alt="" />
+        </div>
+        <div className='datos'>
+          <div className='titile'></div>
+          <div className='description'></div>
+        </div>
+      </div>
+      <div className='conteiner2'>
+        <div className='carrusel'></div>
+      </div>
+
 
     </div>
   )
