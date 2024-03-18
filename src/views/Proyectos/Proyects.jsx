64d9a2f68018db07767a283b4c194370671d7ea3
lucasemanuel <gaitonlucas7@gmail.com>
2024-03-18 17:52:09 -0300

    Implementacion de video de proyecto es la vista de proyectos

--- src/views/Proyectos/Proyects.jsx
+++ src/views/Proyectos/Proyects.jsx
@@ -1,16 +1,34 @@
 import s from "../Proyectos/Proyects.module.css"
+import { useState } from "react"
 export default function Proyects() {
+    const [isPlaying, setIsPlaying] = useState(true);
+
+    const handlerMouseEnter = () => {
+        setIsPlaying(true);
+    };
+
+    const handlerMouseLeave = () => {
+        setIsPlaying(false);
+    }
     return (
         <div className={s.proyects}>
             <section className={s.conteiner1}>
-                <h1>Psiconnection</h1>
+                <div className={s.videoConteiner} onMouseEnter={handlerMouseEnter} onMouseLeave={handlerMouseLeave}>
+                    <video className={s.video} loop muted={true} autoPlay={isPlaying} >
+                        <source src="Videos\2024-03-14 21-33-03.mp4" type="video/mp4" />
+                        Tu navegador no soporta la etiqueta de este video
+
+                    </video>
+                    <div className={isPlaying ? s.overlay : s.ovelayHide}> </div>
+                </div>
+
             </section>
             <section className={s.conteiner2}>
                 <h1>Contries</h1>
-                </section>
+            </section>
             <section className={s.conteiner3}>
                 <h1>foods</h1>
-                </section>
+            </section>
         </div>
     )
 }
